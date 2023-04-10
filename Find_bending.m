134e6dd6dea2e36e3cc1e9a30e98088d7dc1e81b
John DoeOscar72t <100428588@alumnos.uc3m.es>
2023-04-11 01:36:52 +0200

    Considerando también el peso del propio ala

--- Find_bending.m
+++ Find_bending.m
@@ -7,8 +7,8 @@
         forces) whether the index of the for loop is not ahead of this points.
 
     INPUTS: 
-        -> n_span_points: Number of points used to describe wing spanwise characteristics  (integer)
-        -> fuel weight_dist, lift_dist: Distribution of the fuel weight and lift, respectively. Input expected from team A (1xn_span_points double)
+        -> n_span_points: Number of points used to describe wing spanwise characteristics from root to tip  (integer)
+        -> fuel weight_dist, b/2lift_dist: Distribution of the fuel weight and lift, respectively. Input expected from team A (1xn_span_points double)
         -> wing_length: b/2
         -> engine_pos: Nu index corresponding to the engine position  (integer)
         -> landing_gear_pos: Nu index corresponding to the landing gear position  (integer)
@@ -23,13 +23,13 @@
        
 %}
 
-function [bending_moment_dist] = Find_bending (n_span_points, fuel_weight_dist,lift_dist, wing_length, engine_pos,landing_gear_pos, engine_weight, landing_gear_weight, nu)
+function [bending_moment_dist] = Find_bending (n_span_points, fuel_weight_dist,lift_dist, wing_weight_dist,  wing_length, engine_pos,landing_gear_pos, engine_weight, landing_gear_weight, nu)
 
 bending_moment_dist = zeros(1, n_span_points);
 
 for span_pos = 1:100
     iter_range = span_pos:n_span_points;
-    force_distribution = sum((lift_dist(iter_range)-fuel_weight_dist(iter_range)) .* nu(iter_range)) * wing_length;
+    force_distribution = sum((lift_dist(iter_range)-fuel_weight_dist(iter_range)-wing_weight_dist(iter_range))) .* (nu(iter_range)-nu(span_pos)) * wing_length;
 
     if span_pos > engine_pos
         bending_moment_dist(span_pos) = force_distribution;
