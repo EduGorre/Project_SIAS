cef12073fa07f41d2c0819a8b6bc60359bee72e1
AdrianPajaron <adrianpajaron@gmail.com>
2023-04-11 11:54:12 +0200

    Creation of vectors and difinition of weight positions

--- Weight_definition.m
+++ Weight_definition.m
@@ -1,12 +1,10 @@
-function [W_inert] = Weight_definition (c, z, eta, eta_sec1, surface, W_w, W_e, rho_f, ndsa)
-
 %This functions calculates the total wight taking into account both
 %structural and inertial loads.
 %The imputs are:
 
+function [W_total, W_structural, W_landinggear, engine_weight] = Weight_definition (c, surface, W_w, n_span_points, wing_span)
+
 %c: chord provided by the team A.
-%z: height of top surface of wing from cord
-%eta: calculated in main
 %surface: ?
 %W_w: structural weight of the wing
 %W_lgm: inertial weight of the landing gear (Safran) calculated by:
@@ -14,12 +12,8 @@ W_max=172365.1; %Max plane weight at landing
 W_lgm=40+0.16*W_max^0.75+0.019*W_max+1.5*10e-5*W_max^1.5; %Approximation given
 % by Torenbeek for airliner-type aircraft.
 
-%W_e: inertial weight of the engines (GEnx-1B: 6,147 kg or Trent 1000: 5765 kg)
-
-%rho_f: density pof the fuel
-g=9.81; %Gravity
+W_e= 6147; %inertial weight of the engines (GEnx-1B: 6,147 kg or Trent 1000: 5765 kg)
 n_z=3; %Load factor
-%ndsa: fuel tank volume
 
 %% Calculation of the data for the problem
 
@@ -30,21 +24,20 @@ W_structural = W_w/surface*n_z*c; %this needs to be distributed across the surfa
 
 %Inertial weight
 
+
+landing_gear_weight=zeros(1,n_span_points);
 W_landinggear = (W_lgm * n_z) /2; %This weight is located 4.9m from the root
+landing_gear_pos=(4.9/wing_span)*(n_span_points);
+landing_gear_pos=round(landing_gear_pos);
+landing_gear_weight(1,landing_gear_pos)=W_landinggear;
 
-W_engine = W_e*n_z; %This weight is located 9.73m from the root
-
-h=z*2;
-ndsa_sect1= integral (h,eta_sec1(1), eta_sec1(3));
-ndsa_sect23= integral (h,eta(1), eta(3));
-ndsa=ndsa_sect1+ndsa_sect23;
-
-W_fuel = rho_f*g*n_z*c^2*ndsa; %This weight needs to be distributed acroos the fuel tank
+engine_weight = W_e*n_z; %This weight is located 9.73m from the root
+engine_pos=(9.73/wing_span)*(n_span_points);
 
 
 %Total weigth
 
-W_inert = W_structural + W_landinggear + W_engine + W_fuel;
+W_total = W_structural + W_landinggear + engine_weight + W_fuel;
 
 end
 
